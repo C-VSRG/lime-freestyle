5b365564460179090468995cd61fd6e1c7037ded
Rapper GF <84131849+RapperGF@users.noreply.github.com>
2025-05-25 06:05:24 -0400

    Update SDLApplication.cpp

--- project/src/backend/sdl/SDLApplication.cpp
+++ project/src/backend/sdl/SDLApplication.cpp
@@ -401,7 +401,7 @@ namespace lime {
 
 					gamepadsAxisMap[event->caxis.which][event->caxis.axis] = event->caxis.value;
 					gamepadEvent.axisValue = event->caxis.value / (event->caxis.value > 0 ? 32767.0 : 32768.0);
-
+					gamepadEvent.timestamp = event->common.timestamp;
 					GamepadEvent::Dispatch (&gamepadEvent);
 					break;
 
@@ -410,7 +410,7 @@ namespace lime {
 					gamepadEvent.type = GAMEPAD_BUTTON_DOWN;
 					gamepadEvent.button = event->cbutton.button;
 					gamepadEvent.id = event->cbutton.which;
-
+					gamepadEvent.timestamp = event->common.timestamp;
 					GamepadEvent::Dispatch (&gamepadEvent);
 					break;
 
@@ -419,7 +419,7 @@ namespace lime {
 					gamepadEvent.type = GAMEPAD_BUTTON_UP;
 					gamepadEvent.button = event->cbutton.button;
 					gamepadEvent.id = event->cbutton.which;
-
+					gamepadEvent.timestamp = event->common.timestamp;
 					GamepadEvent::Dispatch (&gamepadEvent);
 					break;
 
@@ -429,7 +429,7 @@ namespace lime {
 
 						gamepadEvent.type = GAMEPAD_CONNECT;
 						gamepadEvent.id = SDLGamepad::GetInstanceID (event->cdevice.which);
-
+						gamepadEvent.timestamp = event->common.timestamp;
 						GamepadEvent::Dispatch (&gamepadEvent);
 
 					}
@@ -440,7 +440,7 @@ namespace lime {
 
 					gamepadEvent.type = GAMEPAD_DISCONNECT;
 					gamepadEvent.id = event->cdevice.which;
-
+					gamepadEvent.timestamp = event->common.timestamp;
 					GamepadEvent::Dispatch (&gamepadEvent);
 					SDLGamepad::Disconnect (event->cdevice.which);
 					break;
@@ -575,6 +575,7 @@ namespace lime {
 			keyEvent.keyCode = event->key.keysym.sym;
 			keyEvent.modifier = event->key.keysym.mod;
 			keyEvent.windowID = event->key.windowID;
+			keyEvent.timestamp = event->common.timestamp;
 
 			if (keyEvent.type == KEY_DOWN) {
 
